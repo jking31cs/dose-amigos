474acb52258fb3357530e1dc4da31f3e6a130312
Bobby <jking31cs@gmail.com>
2016-06-22 19:50:04 -0400

    Issue #14  Added OAuth support to Echo stuff.
    Successfully grabbed access token from user object passed into the
    Session object for Alexa, and grabbed user information from it.

--- server/src/main/java/info/doseamigos/meds/MedSpeechlet.java
+++ server/src/main/java/info/doseamigos/meds/MedSpeechlet.java
@@ -5,11 +5,21 @@ import com.amazon.speech.speechlet.*;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
-import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Speechlet for adding medication.
@@ -29,14 +39,45 @@ public class MedSpeechlet implements Speechlet {
 
     @Override
     public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
+        log.info(session.getUser().getUserId());
+        if (session.getUser().getAccessToken() == null) {
+            log.info("The access token is null.");
+            throw new NullPointerException();
+        }
+        String accessToken = session.getUser().getAccessToken();
+        Map<String, String> userInfo = null;
+        try {
+            log.info("Attempting to get user from access token");
+            userInfo = getUserInfo(accessToken);
+
+        } catch (IOException e) {
+            log.error("error occurred grabbing user info from google", e);
+        }
+        log.info("Received user from google.");
+        String name = userInfo == null ? "John Doe" : userInfo.get("name");
         PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
-        outputSpeech.setText("Welcome to Dose Amigos, you can add a new medication.");
+        outputSpeech.setText("Welcome to Dose Amigos " + name + ", you can add a new medication.");
         Reprompt reprompt = new Reprompt();
         reprompt.setOutputSpeech(outputSpeech);
         SimpleCard card = new SimpleCard();
         card.setTitle("Add New Medication");
         card.setContent("Welcome to Dose Amigos, you can add a new medication.");
         return SpeechletResponse.newAskResponse(outputSpeech, reprompt, card);
+    }
+
+    Map<String, String> getUserInfo(String accessToken) throws IOException {
+//        URL url = new URL("https://www.googleapis.com/userinfo/v2/me?access_token=" + "ya29.CjAJAxFJy-WXjpWv6HRIbYy1Exv6suL5diA4JdwTa7xLJqOaC4CAkNKvJ2h_KzJzkWM");
+//        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//        urlConnection.setRequestMethod("GET");
+//        urlConnection.connect();
+        HttpGet httpGet = new HttpGet("https://www.googleapis.com/userinfo/v2/me?access_token=" + "ya29.CjAJAxFJy-WXjpWv6HRIbYy1Exv6suL5diA4JdwTa7xLJqOaC4CAkNKvJ2h_KzJzkWM");
+        HttpClient client = HttpClients.createDefault();
+        HttpResponse response = client.execute(httpGet);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(response.getEntity().getContent(), objectMapper.getTypeFactory()
+            .constructMapType(HashMap.class, String.class, String.class));
+
     }
 
     @Override
---
+++ server/src/test/java/info/doseamigos/meds/MedSpeechletTest.java
@@ -0,0 +1,16 @@
+package info.doseamigos.meds;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by jking31cs on 6/22/16.
+ */
+public class MedSpeechletTest {
+
+    @Test
+    public void testGetUserInfo() throws Exception {
+
+    }
+}
